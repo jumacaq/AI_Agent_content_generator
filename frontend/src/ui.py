2d9d3fed58bbb648f878436d267ea051ecf4e71b
Juan Campos <data_analyst_jcq@outlook.com>
2025-04-22 20:28:03 -0500

    Updating ui.py

--- frontend/src/ui.py
+++ frontend/src/ui.py
@@ -1,6 +1,6 @@
-import os
+#import os
 import json
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import streamlit as st
 #from models.content_generation_models import ContentGeneration
 from src.content_generation_model import ContentGeneration
@@ -8,7 +8,7 @@ from src.generate_content import compute_content
 
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 
 # Set the title of the Streamlit app
@@ -30,7 +30,8 @@ language = st.text_input("Idioma del contenido")
 # Add a button to trigger content generation
 if st.button("Generar Guion"):
     if input_url and new_target_audience and new_tone and language:
-        backend_url = os.getenv("BACKEND_URL", "http://backend:8004/content_generator")
+        backend_url = st.secrets.get("BACKEND_URL", "http://backend:8004/content_generator")
+        #backend_url = os.getenv("BACKEND_URL", "http://backend:8004/content_generator")
         # Create a payload using the ContentGeneration model
         payload = ContentGeneration(
             url=input_url,
